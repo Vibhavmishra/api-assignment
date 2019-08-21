ceb2bafeea64cecbb89185eaf3110e06beb72883
VibhavMishra11 <kk3368184@gmail.com>
2019-08-22 00:27:28 +0530

    Initial server created

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const config = require("./config");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+app.get('/',(req,res)=>{	
+	res.json({message:'Hey I am listening'});
+});
+
+app.use(morgan('dev'));
+
+app.listen(config.port, function () {
+    console.log("Server is running on " + config.port);
+})
